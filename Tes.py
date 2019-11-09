72999eed3c1dfeed3fabf33c0b41d105f3c703e2
Rahul4960 <57430177+Rahul4960@users.noreply.github.com>
2019-11-09 21:15:35 +0800

    Create Tes.py

---
+++ Tes.py
@@ -0,0 +1,33 @@
+
+#!/usr/bin/python
+
+print "Hello World !!!"
+# name = raw_input("R4hu1 4bdre4s5: ")
+Team = int(raw_input("Annonymous: "))
+
+print "Hello ", R4hu1 4ndre4s5
+print "Your Team is : ", Annonymous
+
+#!/usr/bin/python
+
+def SumValue(a, b):
+sum = a + b
+return sum
+
+
+def HelloFunction(Name):
+print "Hello ", R4hu1, ", how are you?"
+return
+
+def PrintNameAndAge(Name, Age = 30):
+print "Name : ",R4hu1,", Team : ",Anonymous
+return
+
+sum = SumValue(12, 7)
+print "Sum is : ", sum
+
+HelloFunction("Tim")
+
+PrintNameAndAge("Tim", 25)
+PrintNameAndAge("Tim")
+
